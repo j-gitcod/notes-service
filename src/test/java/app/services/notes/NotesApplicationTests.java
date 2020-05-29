8a5dfb82db8159ba2cb1a4e7aff85362d6ac2b9f
j-gitcod <j.gitcod@gmail.com>
2020-05-29 15:48:13 -0700

    initial

---
+++ src/test/java/app/services/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package app.services.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
